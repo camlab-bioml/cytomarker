cdda26e9de137dc9c3cb412d075daf84a640f6e0
matt-sd-watson <matt.sd.watson@gmail.com>
2022-09-19 12:28:37 -0400

    Add fa down + fix min cell logic w/ upload

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -315,7 +315,8 @@ test_that("download works as expected", {
     download_data(filepath,
                   list(top_markers = rownames(sce)[1:100]), 
                   plots, heatmap, "fake_path_to_sce", "logcounts",
-                  "seurat_annotations", 24, 2, "no", fake_table)
+                  "seurat_annotations", 24, 2, "no", fake_table,
+                  "fm", NULL, NULL)
     
     # unzip to tempdir and read back
     unzip(filepath, exdir = td)
