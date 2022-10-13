6addb8125fbfb635e86cda66746d33d259fbd9ed
matt-sd-watson <matt.sd.watson@gmail.com>
2022-10-13 17:28:58 -0400

    Make token auto-refresh

--- dropbox.R
+++ dropbox.R
@@ -1,7 +1,8 @@
 library(rdrop2)
 
 # regenerate the token when needed
+# follow this issue for creating long lived tokens: 
+# https://github.com/karthik/rdrop2/issues/201
 rdrop2::drop_auth(new_user = T)
 token <- rdrop2::drop_auth()
-saveRDS(token, "token.rds")
-saveRDS(token, "tests/testthat/token.rds")
+saveRDS(token, file.path("inst", "token.rds"))
