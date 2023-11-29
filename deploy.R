792be19b853c6970a01a5a5ea270eacd47736436
matt-sd-watson <matt.sd.watson@gmail.com>
2023-11-29 11:56:20 -0500

    update bioconductor in GA
    downgrade htmlwidgets to 1.6.2 to be compatible with sortable 0.5.0

--- deploy.R
+++ deploy.R
@@ -2,6 +2,7 @@
 
 library(rsconnect)
 devtools::load_all()
-options(repos=c(BiocManager::repositories(version = "3.17")))
-rsconnect::deployApp(account = 'mwatson-ltri')
+
+options(repos=c(BiocManager::repositories(version = "3.18")))
+rsconnect::deployApp(account = 'camlab')
 
